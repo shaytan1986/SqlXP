db1be99a947a6d87271b2c565a00dbb7b9c75ab9
Shaytan1986 <tetroxide@gmail.com>
2020-04-01 12:04:42 -0600

    Adding SQLCMD build script

---
+++ SQLCMD_BUILD.sql
@@ -0,0 +1,17 @@
+use Master
+go
+set nocount on
+go
+/*
+This script executes the procedures using SQLCMD :r commands
+- YOU MUST RUN THIS IN SQLCMD MODE (in SSMS, go Query -> SQLCMD Mode)
+- YOU MUST REPLACE VALUE OF THE ${dir} VARIABLE WITH THE ROOT DIRECTORY OF THE REPO (where this file lives)
+*/
+:setvar dir "C:\users\tetro\source\repos\Shaytan1986\"
+print '$(dir)'
+:r "$(dir)master\procedures\sp_SetExtendedProperty.sql"
+:r "$(dir)master\procedures\sp_ResolveXPLevelType.sql"
+:r "$(dir)master\procedures\sp_SetObjectXP.sql"
+:r "$(dir)master\procedures\sp_SetMinorXP.sql"
+:r "$(dir)master\procedures\sp_ListXP.sql"
+
