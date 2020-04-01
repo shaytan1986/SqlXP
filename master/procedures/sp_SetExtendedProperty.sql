684e5765098c5e9a99b745f8a77984e9a2a9c491
Shaytan1986 <tetroxide@gmail.com>
2020-03-31 20:43:40 -0600

    sp_SetExtendedProperty

---
+++ master/procedures/sp_SetExtendedProperty.sql
@@ -0,0 +1,179 @@
+use master
+go
+set nocount on
+go
+
+drop proc if exists dbo.sp_SetExtendedProperty
+go
+create or alter proc dbo.sp_SetExtendedProperty
+	@Name nvarchar(128),
+	@Value sql_variant = null,
+	@L0Type	varchar(128) = null,
+	@L0Name	nvarchar(128) = null,
+	@L1Type	varchar(128) = null,
+	@L1Name	nvarchar(128) = null,
+	@L2Type	varchar(128) = null,
+	@L2Name	nvarchar(128) = null,
+	@Action char(1) = 'A',
+	@Debug bit = 0
+as
+begin
+
+declare 
+	@OldValue sql_variant,
+	@OldValueType nvarchar(128),
+	@ValueType nvarchar(128) = convert(nvarchar(128), sql_variant_property(@Value, 'basetype')),
+	@msg nvarchar(2000)
+
+begin try
+
+	select 
+		@OldValue = value,
+		@OldValueType = convert(nvarchar(128), sql_variant_property(value, 'basetype'))
+	from sys.fn_listextendedproperty
+	(
+		@Name, 
+		@L0Type, 
+		@L0Name, 
+		@L1Type, 
+		@L1Name, 
+		@L2Type, 
+		@L2Name
+	)
+
+	if @OldValueType != @ValueType
+		raiserror('WARNING: Type Change Detected. Old Type: %s, New Type: %s', 10, 1, @OldValueType, @ValueType)
+
+	/*****************************
+	DROPS
+	*****************************/
+	if @Action = 'D'
+		begin
+			if @OldValue is not null
+				exec sys.sp_dropextendedproperty
+					@Name = @Name,
+					@Level0Type = @L0Type,
+					@Level0Name = @L0Name,
+					@Level1Type = @L1Type,
+					@Level1Name = @L1Name,
+					@Level2Type = @L2Type,
+					@Level2Name = @L2Name
+			else if @Debug = 1
+				raiserror('No value for @Name: %s exists to Drop.', 10, 1, @Name)
+		end
+
+	/*****************************
+	UPDATES
+	*****************************/
+	if @Action = 'U'
+	begin
+		if @OldValue is null
+			begin
+				select @Action = 'A'
+
+				if @Debug = 1
+					raiserror('No value for @Name: %s exists to Update; changing @Action to "A".', 10, 1, @Name)
+			end
+		else if @OldValue != @Value
+			begin
+
+				exec sys.sp_updateextendedproperty
+					@Name = @Name,
+					@Value = @Value,
+					@Level0Type = @L0Type,
+					@Level0Name = @L0Name,
+					@Level1Type = @L1Type,
+					@Level1Name = @L1Name,
+					@Level2Type = @L2Type,
+					@Level2Name = @L2Name
+			end
+		else if @Debug = 1
+			raiserror('Value exists for @Name: %s but it matches the existing value; no action taken.', 10, 1, @Name)
+	end
+
+	/*****************************
+	ADDS
+	*****************************/
+	if @Action = 'A'
+	begin
+		if @OldValue is null
+			exec sys.sp_addextendedproperty
+				@Name = @Name,
+				@Value = @Value,
+				@Level0Type = @L0Type,
+				@Level0Name = @L0Name,
+				@Level1Type = @L1Type,
+				@Level1Name = @L1Name,
+				@Level2Type = @L2Type,
+				@Level2Name = @L2Name
+		else if @Debug = 1
+			raiserror('Value alrady exists for @Name: %s; no action taken', 10, 1, @Name)
+	end
+
+	if @Debug = 1
+		select 
+			Action = @Action, 
+			OldValue = @OldValue,
+			OldValueType = @OldValueType,
+			NewValue = @Value,
+			NewValueType = @ValueType
+		from sys.fn_listextendedproperty
+		(
+			@Name, 
+			@L0Type, 
+			@L0Name, 
+			@L1Type, 
+			@L1Name, 
+			@L2Type, 
+			@L2Name
+		)
+
+end try
+begin catch
+	
+	if @@trancount > 0
+	    rollback tran
+	;throw
+	
+end catch
+end
+return
+go
+exec sys.sp_ms_marksystemobject N'dbo.sp_SetExtendedProperty'
+go
+
+exec dbo.sp_SetExtendedProperty
+    @Name = N'name', -- nvarchar(128)
+    @Value = 'Value', -- sql_variant
+    @L0Type = 'SCHEMA', -- varchar(128)
+    @L0Name = 'dbo', -- nvarchar(128)
+    @L1Type = 'PROCEDURE', -- varchar(128)
+    @L1Name = 'sp_SetExtendedProperty', -- nvarchar(128)
+    @L2Type = null, -- varchar(128)
+    @L2Name = null, -- nvarchar(128)
+    @Action = 'A', -- char(1)
+    @Debug = 1 -- bit
+
+exec dbo.sp_SetExtendedProperty
+    @Name = N'name', -- nvarchar(128)
+    @Value = 'ValueChanged', -- sql_variant
+    @L0Type = 'SCHEMA', -- varchar(128)
+    @L0Name = 'dbo', -- nvarchar(128)
+    @L1Type = 'PROCEDURE', -- varchar(128)
+    @L1Name = 'sp_SetExtendedProperty', -- nvarchar(128)
+    @L2Type = null, -- varchar(128)
+    @L2Name = null, -- nvarchar(128)
+    @Action = 'U', -- char(1)
+    @Debug = 1 -- bit
+
+exec dbo.sp_SetExtendedProperty
+    @Name = N'name', -- nvarchar(128)
+    @Value = null, -- sql_variant
+    @L0Type = 'SCHEMA', -- varchar(128)
+    @L0Name = 'dbo', -- nvarchar(128)
+    @L1Type = 'PROCEDURE', -- varchar(128)
+    @L1Name = 'sp_SetExtendedProperty', -- nvarchar(128)
+    @L2Type = null, -- varchar(128)
+    @L2Name = null, -- nvarchar(128)
+    @Action = 'D', -- char(1)
+    @Debug = 1 -- bit
